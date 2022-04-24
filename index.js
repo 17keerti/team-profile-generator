72fb8ca6e46ab81cfec368f5cd66bb8627b55bff
Keerti Chaudhary <17keerti@gmail.com>
2022-04-24 12:57:31 -0700

    add promises in index.js file

--- index.js
+++ index.js
@@ -9,184 +9,197 @@ const fileName = ("./dist/generatedHtml.html");
 const myTeamArray = [];
 
 function addManager() {
-  return inquirer.prompt([
-    {
-      type: 'input',
-      message: "What is the team manager's name?",
-      name: 'name',
-    },
-    {
-      type: 'input',
-      message: "What is the team manager's id ?",
-      name: 'id',
-    },
-    {
-      type: 'input',
-      message: "What is the team manager's email ?",
-      name: 'email',
-    },
-    {
-      type: 'input',
-      message: "What is the team manager's office number?",
-      name: 'officeNumber',
-    }
-  ])
-    .then((managerResponse) => {
-      const { name, id, email, officeNumber } = managerResponse;
-      const manager = new Manager(name, id, email, officeNumber);
+  return new Promise((resolve, reject) => {
+    inquirer.prompt([
+      {
+        type: 'input',
+        message: "What is the team manager's name?",
+        name: 'name',
+      },
+      {
+        type: 'input',
+        message: "What is the team manager's id ?",
+        name: 'id',
+      },
+      {
+        type: 'input',
+        message: "What is the team manager's email ?",
+        name: 'email',
+      },
+      {
+        type: 'input',
+        message: "What is the team manager's office number?",
+        name: 'officeNumber',
+      }
+    ])
+      .then((managerResponse) => {
+        const { name, id, email, officeNumber } = managerResponse;
+        const manager = new Manager(name, id, email, officeNumber);
 
-      myTeamArray.push(manager);
-      console.log(manager);
+        myTeamArray.push(manager);
 
-      inquirer.prompt([
-        {
-          type: 'confirm',
-          name: 'confirmMoreEmployees',
-          message: 'Would you like to add more team members?',
-          default: false
-        }
-      ])
-        .then((response) => {
-          if (response.confirmMoreEmployees) {
-            return addEmployee();
-          } else {
-            return myTeamArray;
+        inquirer.prompt([
+          {
+            type: 'confirm',
+            name: 'confirmMoreEmployees',
+            message: 'Would you like to add more team members?',
+            default: false
           }
-        })
-    })
-
+        ])
+          .then((response) => {
+            if (response.confirmMoreEmployees) {
+              var addEmployeeP = addEmployee();
+              addEmployeeP.then((data) => {
+                resolve(data)
+              });
+            } else {
+              resolve(myTeamArray);
+            }
+          })
+      })
+  });
 }
 
 
 function addEmployee() {
-  return inquirer.prompt([
-    {
-      type: 'list',
-      message: "Which type of team member would you like to add ?",
-      name: 'role',
-      choices: ['Engineer', 'Intern', 'I dont want to add any more team members'],
-      default: 'I dont want to add any more team members',
-    }
-  ])
-    .then((roleResponse) => {
-      if (roleResponse.role === 'Engineer') {
-        addEngineer();
-      } else if (roleResponse.role === 'Intern') {
-        addIntern();
+  return new Promise((resolve, reject) => {
+
+    inquirer.prompt([
+      {
+        type: 'list',
+        message: "Which type of team member would you like to add ?",
+        name: 'role',
+        choices: ['Engineer', 'Intern', 'I dont want to add any more team members'],
+        default: 'I dont want to add any more team members',
       }
-    })
+    ])
+      .then((roleResponse) => {
+        if (roleResponse.role === 'Engineer') {
+          var addEngineerP = addEngineer();
+          addEngineerP.then((data) => {
+            resolve(data);
+          });
+        } else if (roleResponse.role === 'Intern') {
+          var addInternP = addIntern();
+          addInternP.then((data) => {
+            resolve(data);
+          })
+        }
+      });
+  });
 }
 
 function addEngineer() {
-  return inquirer.prompt([
-    {
-      type: 'input',
-      message: "What is the your Engineer's name?",
-      name: 'name',
-    },
-    {
-      type: 'input',
-      message: "What is the your Engineer's id ?",
-      name: 'id',
-    },
-    {
-      type: 'input',
-      message: "What is the your Engineer's email ?",
-      name: 'email',
-    },
-    {
-      type: 'input',
-      message: "What is the your Engineer's Github username ?",
-      name: 'githubUsername',
-    }
-  ])
-    .then((engineerResponse) => {
-      const { name, id, email, githubUsername } = engineerResponse;
-      const engineer = new Engineer(name, id, email, githubUsername);
+  return new Promise((resolve, reject) => {
 
-      myTeamArray.push(engineer);
-      console.log(engineer);
-      inquirer.prompt([
-        {
-          type: 'confirm',
-          name: 'confirmMoreEmployees',
-          message: 'Would you like to add more team members?',
-          default: false
-        }
-      ])
-        .then((response) => {
-          if (response.confirmMoreEmployees) {
-            return addEmployee();
-          } else {
-            return myTeamArray;
+    inquirer.prompt([
+      {
+        type: 'input',
+        message: "What is the your Engineer's name?",
+        name: 'name',
+      },
+      {
+        type: 'input',
+        message: "What is the your Engineer's id ?",
+        name: 'id',
+      },
+      {
+        type: 'input',
+        message: "What is the your Engineer's email ?",
+        name: 'email',
+      },
+      {
+        type: 'input',
+        message: "What is the your Engineer's Github username ?",
+        name: 'githubUsername',
+      }
+    ])
+      .then((engineerResponse) => {
+        const { name, id, email, githubUsername } = engineerResponse;
+        const engineer = new Engineer(name, id, email, githubUsername);
+
+        myTeamArray.push(engineer);
+        inquirer.prompt([
+          {
+            type: 'confirm',
+            name: 'confirmMoreEmployees',
+            message: 'Would you like to add more team members?',
+            default: false
           }
-        })
-    })
+        ])
+          .then((response) => {
+            if (response.confirmMoreEmployees) {
+              var addEmployeeP = addEmployee();
+              addEmployeeP.then((data) => {
+                resolve(data)
+              });
+            } else {
+              resolve(myTeamArray);
+            }
+          })
+      })
+  });
 }
 
 
 function addIntern() {
-  return inquirer.prompt([
-    {
-      type: 'input',
-      message: "What is the your Intern's name?",
-      name: 'name',
-    },
-    {
-      type: 'input',
-      message: "What is the your Intern's id ?",
-      name: 'id',
-    },
-    {
-      type: 'input',
-      message: "What is the your Intern's email ?",
-      name: 'email',
-    },
-    {
-      type: 'input',
-      message: "What is your Intern's School ?",
-      name: 'school',
-    }
-  ])
-    .then((internResponse) => {
-      const { name, id, email, school } = internResponse;
-      const intern = new Intern(name, id, email, school);
+  return new Promise((resolve, reject) => {
 
-      myTeamArray.push(intern);
-      console.log(intern);
-      inquirer.prompt([
-        {
-          type: 'confirm',
-          name: 'confirmMoreEmployees',
-          message: 'Would you like to add more team members?',
-          default: false
-        }
-      ])
-        .then((response) => {
-          if (response.confirmMoreEmployees) {
-            return addEmployee();
-          } else {
-            return myTeamArray;
+    inquirer.prompt([
+      {
+        type: 'input',
+        message: "What is the your Intern's name?",
+        name: 'name',
+      },
+      {
+        type: 'input',
+        message: "What is the your Intern's id ?",
+        name: 'id',
+      },
+      {
+        type: 'input',
+        message: "What is the your Intern's email ?",
+        name: 'email',
+      },
+      {
+        type: 'input',
+        message: "What is your Intern's School ?",
+        name: 'school',
+      }
+    ])
+      .then((internResponse) => {
+        const { name, id, email, school } = internResponse;
+        const intern = new Intern(name, id, email, school);
+
+        myTeamArray.push(intern);
+
+        inquirer.prompt([
+          {
+            type: 'confirm',
+            name: 'confirmMoreEmployees',
+            message: 'Would you like to add more team members?',
+            default: false
           }
-        })
-    })
-}
-
-function writeToFile(filename, data) {
-  const templateHtml =templateHtml(data);
-  fs.writeFile(filename, templateHtml, (err) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log('Successfully file has been created!');
-    }
-  })
+        ])
+          .then((response) => {
+            if (response.confirmMoreEmployees) {
+              var addEmployeeP = addEmployee();
+              addEmployeeP.then((data) => {
+                resolve(data)
+              });
+            } else {
+              resolve(myTeamArray);
+            }
+          })
+      })
+  });
 }
 
 function init() {
-  addManager();
-  writeToFile(fileName, myTeamArray); 
+  var test = addManager();
+  test.then((data) => {
+    console.log(data);
+  });
 }
-
 
 init();
--- src/templateHtml.js
+++ src/templateHtml.js
@@ -1,7 +1,66 @@
-function generateHtml()  {
-return `<!DOCTYPE html>
-<html lang="en">
-<head>
+function generateManagerCard(Manager) {
+    return `
+    <div class="card" style="width: 18rem;">
+  <div class="card-header bg-primary text-white">
+      <h2 class="card-title">Name</h2>
+      <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i> Manager : ${Manager.name}</h3>
+  </div>
+  <div class="card-body">
+      <ul class="list-group">
+          <li class="list-group-item">ID: ${Manager.id}</li>
+          <li class="list-group-item">Email: ${Manager.email}<a href=""></a></li>
+          <li class="list-group-item">Office number: ${Manager.officeNumber}</li>
+      </ul>
+  </div>
+  </div>
+  `;
+  }
+  
+  
+  function generateEngineerCard(Engineer) {
+    return `
+    <div class="card" style="width: 18rem;">
+  <div class="card-header bg-primary text-white">
+      <h2 class="card-title">${Engineer.name}</h2>
+      <h3 class="card-title"><i class="fas fa-glasses mr-2"></i> Engineer</h3>
+  </div>
+  <div class="card-body">
+      <ul class="list-group">
+          <li class="list-group-item">ID: ${Engineer.id}</li>
+          <li class="list-group-item">Email: ${Engineer.email}<a href=""></a></li>
+          <li class="list-group-item">Github: ${Engineer.githubUsername}</li>
+      </ul>
+  </div>
+  </div>
+  `;
+  }
+  
+  
+  function generateInternCard(Intern) {
+    return `
+    <div class="card" style="width: 18rem;">
+      <div class="card-header bg-primary text-white">
+     <h2 class="card-title">Name${Intern.name}</h2>
+    <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i> Intern</h3>
+  </div>
+  <div class="card-body">
+    <ul class="list-group">
+        <li class="list-group-item">ID: ${Intern.id}</li>
+        <li class="list-group-item">Email: ${Intern.email}<a href=""></a></li>
+        <li class="list-group-item">School: ${Intern.school}</li>
+    </ul>
+  </div>
+  </div>
+  `;
+  }
+  
+  
+  
+  
+  function generateHtml() {
+    return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,8 +68,8 @@ return `<!DOCTYPE html>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link rel="stylesheet" href="./style/style.css">
   <title>My Team</title>
-</head>
-<body>
+  </head>
+  <body>
   
   <div class="container-fluid">
     <div class="row">
@@ -18,62 +77,19 @@ return `<!DOCTYPE html>
             <h1 class="text-center text-white">My Team</h1>
         </div>
     </div>
-</div>
-<div class="container">
+  </div>
+  <div class="container">
     <div class="row">
         <div class="row team-area col-12 d-flex justify-content-center">
-            <!-- Manager card -->
-<div class="card" style="width: 18rem;">
-  <div class="card-header bg-primary text-white">
-      <h2 class="card-title">Name</h2>
-      <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i> Manager</h3>
+  
+        
   </div>
-  <div class="card-body">
-      <ul class="list-group">
-          <li class="list-group-item">ID:</li>
-          <li class="list-group-item">Email: <a href=""></a></li>
-          <li class="list-group-item">Office number: </li>
-      </ul>
-  </div>
-</div>
-
-<!-- Engineer card -->
-
-<div class="card" style="width: 18rem;">
-  <div class="card-header bg-primary text-white">
-      <h2 class="card-title">Name</h2>
-      <h3 class="card-title"><i class="fas fa-glasses mr-2"></i> Engineer</h3>
-  </div>
-  <div class="card-body">
-      <ul class="list-group">
-          <li class="list-group-item">ID:</li>
-          <li class="list-group-item">Email: <a href=""></a></li>
-          <li class="list-group-item">Github: </li>
-      </ul>
-  </div>
-</div>
-
-<!-- Intern card -->
-
-<div class="card" style="width: 18rem;">
-<div class="card-header bg-primary text-white">
-    <h2 class="card-title">Name</h2>
-    <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i> Intern</h3>
-</div>
-<div class="card-body">
-    <ul class="list-group">
-        <li class="list-group-item">ID:</li>
-        <li class="list-group-item">Email: <a href=""></a></li>
-        <li class="list-group-item">School: </li>
-    </ul>
-</div>
-</div>
-</div>
         </div>
     </div>
-</div>
+  </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-</body>
-</html>
-`
-}
+  </body>
+  </html>
+  `}
+  
+  module.exports = generateHtml();
