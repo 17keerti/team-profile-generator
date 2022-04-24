4759fb4e01377f54e6d0d85a25968397324e1ab5
Keerti Chaudhary <17keerti@gmail.com>
2022-04-24 11:00:56 -0700

    test for engineer class

---
+++ tests/Engineer.test.js
@@ -0,0 +1,30 @@
+const Engineer = require("../lib/Engineer")
+
+describe("Employee", () => {
+  describe('Initialization', () => {
+    describe("github", () => {
+      it("can set github username using constructor", () => {
+        const github = "17keerti";
+        const engineer= new Engineer("Keerti",12,"17keerti@gmail.com", github);
+        expect(engineer.github).toEqual(github);
+  
+      });
+    });
+    describe("getGithub", () => {
+      it("can get github username", () => {
+        const github = "17keerti";
+        const engineer = new Engineer("Keerti",12,"17keerti@gmail.com", github);
+        expect(engineer.getGithub()).toEqual(github);
+  
+      });
+    });
+    describe("getRole", () => {
+      it("should return string Engineer", () => {
+        const role = "Engineer";
+        const engineer = new Engineer("Keerti",12,"17keerti@gmail.com", "17keerti");
+        expect(engineer.getRole()).toEqual(role);
+  
+      });
+    });
+  });
+});
